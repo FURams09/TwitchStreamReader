839e2299b36e993879455b43b6e0f1ebae42b30e
Greg <padin.greg@gmail.com>
2018-12-02 17:17:55 -0500

    Initial Commit
    Initial Commit of Twitch reader

---
+++ test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 14:52:44 2018
+
+@author: greg
+"""
+
+import cv2;
+import numpy as np
+import time
+import streamlink
+
+# use live streamer to figure out the stream info
+streams = streamlink.streams("http://twitch.tv/trymacs")
+stream = streams['best']
+# open our out file. 
+fname = "test.mpg"
+vid_file = open(fname,"wb")
+# dump from the stream into an mpg file -- get a buffer going
+fd = stream.open()
+for i in range(0,2*2048):
+    if i%256==0:
+        print ("Buffering...")
+    new_bytes = fd.read(1024)
+    vid_file.write(new_bytes)
+# open the video file from the begining
+print ("Done buffering.")
+cam = cv2.VideoCapture(fname)
+while True:
+    ret, img = cam.read()                   
+    try:
+        if ret:
+            gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            blur= cv2.GaussianBlur(gray_image, (5, 5), 0)
+            # canny = cv2.Canny(blur, 50, 180)
+            
+            cv2.imshow('live_img',blur)
+            cv2.resizeWindow('live_img', 600, 600)
+       
+    except:
+        print ("DERP")
+        continue
+    if (0xFF & cv2.waitKey(5) == 27) or img.size == 0:
+        break
+    time.sleep(0.05)
+    # dump some more data to the stream so we don't run out. 
+    new_bytes = fd.read(1024*16)
+    vid_file.write(new_bytes)
+vid_file.close()
+fd.close()
