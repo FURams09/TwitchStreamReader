055997d4426dd699207874e1b1c2086a1a9dd364
FURams09 <padin.greg@gmail.com>
2018-12-10 18:42:28 -0500

    ClassifyImages now works on S3

--- classifyimages.py
+++ classifyimages.py
@@ -16,7 +16,7 @@ s3client = boto3.client('s3',
                         aws_access_key_id=config['S3_BUCKET']['accesskey'],
                         aws_secret_access_key=config['S3_BUCKET']['accesssecret'],)
 
-specificfile = 'starcraft 2_181207_062429.zip'
+specificfile = 'fortnite_181210_142240.zip'
 
 # check if file exits locally, if not grab it from s3
 clips = s3client.list_objects(
@@ -36,13 +36,27 @@ temppath = os.path.join(config['DIRECTORY']['temp'],
 if not os.path.exists(temppath):
     os.makedirs(temppath)
 
+
+frameTypes = {
+    "positive": [[103, 112], 'this is gameplay'],
+    "bus": [98, 'player is on bus waiting to drop'],
+    "negative": [[102, 110], 'this has nothing to do with fortnite'],
+    "lobby": [108, 'player is in a lobby or on a loading screen'],
+    "waiting": [119, 'player is waiting for a game to fill'],
+    "unsure": [117, 'you aren\'t sure how to categorize this photo'],
+}
+
 fileno = 0
 startIndex = 0  # 875
 bail = False
 print('loading clips')
 with io.BytesIO(clip['Body'].read()) as tf:
     tf.seek(0)
+    tempdir = f'data/temp/frames'
+    if not os.path.exists(tempdir):
+        os.makedirs(tempdir)
     with zipfile.ZipFile(tf, mode='r') as zf:
+        print('clips loaded')
         noOfClips = len(zf.infolist())
         for zippedfile in zf.infolist():
             fileno += 1
@@ -58,20 +72,9 @@ with io.BytesIO(clip['Body'].read()) as tf:
 
             # positive
 
-            if not os.path.exists(f'data/temp/frames'):
-                os.makedirs(f'data/temp/frames')
-            if not os.path.exists(f'data/temp/frames/positive'):
-                os.makedirs(f'data/temp/frames/positive')
-            if not os.path.exists(f'data/temp/frames/bus'):
-                os.makedirs(f'data/temp/frames/bus')
-            if not os.path.exists(f'data/temp/frames/lobby'):
-                os.makedirs(f'data/temp/frames/lobby')
-            if not os.path.exists(f'data/temp/frames/negative'):
-                os.makedirs(f'data/temp/frames/negative')
-            if not os.path.exists(f'data/temp/frames/waiting'):
-                os.makedirs(f'data/temp/frames/waiting')
-            if not os.path.exists(f'data/temp/frames/unsure'):
-                os.makedirs(f'data/temp/frames/unsure')
+            for categories in frameTypes:
+                if not os.path.exists(f'{tempdir}/{categories}'):
+                    os.makedirs(f'{tempdir}/{categories}')
 
             grabevery = 50  # Show an image for judgement every x frames.
             width, height = image.shape[:2]
@@ -79,160 +82,64 @@ with io.BytesIO(clip['Body'].read()) as tf:
             while success and not bail:
                 if count % grabevery == 0:
                     textimage = image.copy()
+                    textTop = 20
                     cv2.putText(textimage, f'{fileno} of { noOfClips} clips',
                                 (0, 20), font, .6, (255, 0, 20), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter G or P  if this is gameplay',
-                                (int(width/2), 20), font, .6, (0, 0, 20), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter F or N  if this is a negative',
-                                (int(width/2),  45), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter B if the player is flying on the bus',
-                                (int(width/2), 70), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter L if the player is in the lobby or on a loading screen',
-                                (int(width/2),  95), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter W if the player is waiting for a game to fill',
-                                (int(width/2),  120), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter U if you can\'t quite classify',
-                                (int(width/2), 145), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
+                    for categories in frameTypes:
+                        textTop += 20
+                        if isinstance(frameTypes[categories][0], list):
+                            keys = map(lambda x: chr(
+                                x), frameTypes[categories][0])
+                            keys = ' or '.join(keys)
+
+                        else:
+                            keys = chr(frameTypes[categories][0])
+                        message = f'Press {keys} if {frameTypes[categories][1]}'
+                        cv2.putText(textimage, message,
+                                    (int(width/2), textTop), font, .6, (255, 255, 255), 2, cv2.LINE_AA)
+                    textTop += 20
                     cv2.putText(textimage, 'Enter Q to quit',
-                                (int(width/2), 165), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
+                                (int(width/2), textTop), font, .6, (255, 255, 255), 2, cv2.LINE_AA)
                     cv2.imshow("Image", textimage)
 
                     picResponse = cv2.waitKey()
-                    print(picResponse)
-                    frameNo = int(count/grabevery)
-                    clipname = f'{os.path.splitext(tempfilename)[0]}_{int(frameNo)}'
-
-                    if picResponse == 103 or picResponse == 112:  # G or P means it's a valid game screen
-                        cv2.imwrite(f"data/temp/frames/positive/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 98:  # B means they're on bus waiting to drop in
-                        cv2.imwrite(f"data/temp/frames/bus/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 102 or picResponse == 110:  # F or N means they failed and this goes to negative
-                        cv2.imwrite(f"data/temp/frames/negative/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 108:  # L means they are Lobby
-                        cv2.imwrite(f"data/temp/frames/lobby/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 119:  # W means they are Waiting to fill the party
-                        cv2.imwrite(f"data/temp/frames/waiting/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 117:  # U means they are Unsure how to classify this image
-                        cv2.imwrite(f"data/temp/frames/unsure/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
                     if picResponse == 113:  # Q means Quit
                         bail = True
                         break
+                    frameNo = int(count/grabevery)
+                    clipname = f'{os.path.splitext(tempfilename)[0]}_{int(frameNo)}'
+                    keyFound = False
+                    for key in frameTypes:
+                        if isinstance(frameTypes[key][0], list):
+                            if picResponse in frameTypes[key][0]:
+                                keyFound = True
+                        else:
+                            keyFound = picResponse == frameTypes[key][0]
+                        if keyFound:
+                            cv2.imwrite(f"{tempdir}/{key}/{clipname}.jpg",
+                                        image)
+                            break
                 if bail:
                     break
                 success, image = vidcap.read()
                 count += 1
             if bail:
                 break
-            # TODO: Create a processed zip and move every 100 clips over so this can be done in batches.
+# TODO: Create a processed zip and move every 100 clips over so this can be done in batches.
+vidcap.release()
+if not bail:
+    for dir in os.listdir(tempdir):
+        print(dir, '--')
+        for root, dirs, files in os.walk(os.path.join(tempdir, dir)):
+            for fname in files:
+                s3client.upload_file(
+                    os.path.join(tempdir, dir, fname), config['S3_BUCKET']['bucket'], f'frames/{dir}/{fname}')
+    # s3 move zip file to processed
+    oldKey = config['S3_BUCKET']['bucket'] + '/' + zipfilename
+    print(zipfilename)
+    s3client.copy_object(Bucket=config['S3_BUCKET']['bucket'], CopySource=oldKey,
+                         Key=zipfilename.replace('/unsorted/', '/processed/'))
+    s3client.delete_object(
+        Bucket=config['S3_BUCKET']['bucket'], Key=zipfilename)
+shutil.rmtree(tempdir)
 # upload sorted files to s3 and then clean up the folders
-if False:
-
-    # Specify a file in data/clips to pull from or else this will just grab a random file
-    specificfile = 'fortnite_181207_164325.zip'
-
-    #
-    startIndex = 875
-    temppath = os.path.join(
-        config['DIRECTORY']['temp'], f'{zipfilename}_{now}')
-    if not os.path.exists(temppath):
-        os.makedirs(temppath)
-    fileno = 0
-    with zipfile.ZipFile(zipdir) as zf:
-        noOfClips = len(zf.infolist())
-        for zippedfile in zf.infolist():
-            fileno += 1
-            if fileno < startIndex:
-                continue
-            zf.extract(zippedfile, temppath)
-            tempfilename = zippedfile.filename
-            tempfile = os.path.join(temppath, tempfilename)
-            print(tempfile)
-            vidcap = cv2.VideoCapture(tempfile)
-            success, image = vidcap.read()
-            count = 0
-
-            # positive
-
-            if not os.path.exists(f'data/frames'):
-                os.makedirs(f'data/frames')
-            if not os.path.exists(f'data/frames/positive'):
-                os.makedirs(f'data/frames/positive')
-            if not os.path.exists(f'data/frames/bus'):
-                os.makedirs(f'data/frames/bus')
-            if not os.path.exists(f'data/frames/lobby'):
-                os.makedirs(f'data/frames/lobby')
-            if not os.path.exists(f'data/frames/negative'):
-                os.makedirs(f'data/frames/negative')
-            if not os.path.exists(f'data/frames/waiting'):
-                os.makedirs(f'data/frames/waiting')
-            if not os.path.exists(f'data/frames/unsure'):
-                os.makedirs(f'data/frames/unsure')
-
-            grabevery = 50  # Show an image for judgement every x frames.
-            width, height = image.shape[:2]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            while success:
-                if count % grabevery == 0:
-                    textimage = image.copy()
-                    cv2.putText(textimage, f'{fileno} of { noOfClips} clips',
-                                (0, 20), font, .6, (255, 0, 20), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter G or P  if this is gameplay',
-                                (int(width/2), 20), font, .6, (0, 0, 20), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter F or N  if this is a negative',
-                                (int(width/2),  45), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter B if the player is flying on the bus',
-                                (int(width/2), 70), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter L if the player is in the lobby or on a loading screen',
-                                (int(width/2),  95), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter W if the player is waiting for a game to fill',
-                                (int(width/2),  120), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter U if you can\'t quite classify',
-                                (int(width/2), 145), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.putText(textimage, 'Enter Q to quit',
-                                (int(width/2), 165), font, .6, (0, 0, 0), 2, cv2.LINE_AA)
-                    cv2.imshow("Image", textimage)
-
-                    picResponse = cv2.waitKey()
-                    print(picResponse)
-                    frameNo = int(count/grabevery)
-                    clipname = f'{os.path.splitext(tempfilename)[0]}_{int(frameNo)}'
-
-                    if picResponse == 103 or picResponse == 112:  # G or P means it's a valid game screen
-                        cv2.imwrite(f"data/frames/positive/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 98:  # B means they're on bus waiting to drop in
-                        cv2.imwrite(f"data/frames/bus/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 102 or picResponse == 110:  # F or N means they failed and this goes to negative
-                        cv2.imwrite(f"data/frames/negative/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 108:  # L means they are Lobby
-                        cv2.imwrite(f"data/frames/lobby/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 119:  # W means they are Waiting to fill the party
-                        cv2.imwrite(f"data/frames/waiting/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    elif picResponse == 117:  # U means they are Unsure how to classify this image
-                        cv2.imwrite(f"data/frames/unsure/{clipname}.jpg",
-                                    image)     # save frame as JPEG file
-                    if picResponse == 113:  # Q means Quit
-                        bail = True
-                        break
-                if bail:
-                    break
-                success, image = vidcap.read()
-                count += 1
-
-            if bail:
-                break
-    vidcap.release()
-    if not bail:
-        shutil.move(f'data/clips/{zipfilename}',
-                    f'data/clips/processed/{zipfilename}')
-    shutil.rmtree(temppath)
